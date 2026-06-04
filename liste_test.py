6066cc5192359d543865e285a311adead79faba2
ilaydatk4 <ilaydatk4@gmail.com>
2026-06-04 13:33:57 +0300

    The Architect 3.gun: liste ve sozluk yapıları insa edildi

---
+++ liste_test.py
@@ -0,0 +1,3 @@
+mimarlar = ['Ilayda','Cane','Mimar_Ahmet']
+print('Ilk Mimar:',mimarlar[0])
+print('Mimar Sayisi:', len(mimarlar))
---
+++ sozluk_test.py
@@ -0,0 +1,4 @@
+proje = {'adi' : 'The Architect' , 'sure' : '30 Gun' , 'durum' : 'Aktif'}
+print('proje Adi:',proje['adi'])
+print('Kaç Günlük:', proje['sure'])
+print('Aktiflik Durumu:' , proje['durum'])
